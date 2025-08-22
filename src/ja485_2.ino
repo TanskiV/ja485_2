db81af9895acd42a41cd5f0df219410a835f2602
Vladimir Tanski <38607979+TanskiV@users.noreply.github.com>
2025-08-22 15:28:19 +0300

    Avoid blocking setup on serial connection

--- src/ja485_2.ino
+++ src/ja485_2.ino
@@ -19,7 +19,7 @@ unsigned long lastServerTime = 0;
 
 void setup() {
     Serial.begin(9600);
-    while (!Serial);
+    waitFor(Serial.isConnected, 5000);
 
     Serial1.begin(9600, SERIAL_8N1);
     Serial.println("Modbus Initialized.");
